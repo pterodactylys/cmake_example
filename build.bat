686b2f2cb53cddd9c75622b623e80809232f0c7c
pterodactylys <andreicsgoawp@gmail.com>
2025-03-22 22:51:49 +0300

    Fixed errors

--- build.bat
+++ build.bat
@@ -4,15 +4,16 @@ set BUILD_TYPE=Ninja
 set BUILD_SUFFIX=ninja
 
 chcp 65001
-
-set BUILD_FOLDER=build_%BUILD_SUFFIX%
+set PROJECT_NAME=shell_sort
+set BUILD_FOLDER=build_ninja
 set SOURCE_FOLDER=cmake_example
 
 if not exist %BUILD_FOLDER% mkdir %BUILD_FOLDER%
 
 cd %BUILD_FOLDER%
 
-cmake -G %BUILD_TYPE%
-cmake --build .
+cmake ..
+%BUILD_SUFFIX%
+cd %PROJECT_NAME%
 
-copy ..\%SOURCE_FOLDER%\shell_sort\run_shell_sort.bat .\shell_sort
+%PROJECT_NAME%.exe
